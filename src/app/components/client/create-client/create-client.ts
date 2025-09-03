9438af87256a689afc5e38cd6bae05a08889972c
IngRonal <ingronal10@gmail.com>
2025-09-03 12:23:02 -0500

    avance en clase

--- src/app/components/client/create-client/create-client.ts
+++ src/app/components/client/create-client/create-client.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-create-client',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, InputTextModule],
   templateUrl: './create-client.html',
   styleUrl: './create-client.css'
 })
 export class CreateClient {
+
+  form: FormGroup;
+
+ constructor(
+   private fb: FormBuilder,
+   private router: Router,
+ ) {
+  this.form = this.fb.group({
+    name: ['', Validators.required],
+    address: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    status: ['', Validators.required]
+  });
+ }
 
 }
--- src/app/components/client/get-all-client/get-all-client.ts
+++ src/app/components/client/get-all-client/get-all-client.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { ClientI } from '../../../models/client';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-get-all-client',
-  imports: [],
+  imports: [TableModule, CommonModule, ButtonModule, RouterModule],
   templateUrl: './get-all-client.html',
   styleUrl: './get-all-client.css'
 })
 export class GetAllClient {
+  
+   clients: ClientI[] = [
+     {
+       id: 1,
+       name: 'John Doe',
+       address: '123 Main St',
+       phone: '123-456-7890',
+       email: 'john.doe@example.com',
+       status: 'ACTIVE'
+     },
+     {
+       id: 2,
+       name: 'Jane Smith',
+       address: '456 Elm St',
+       phone: '987-654-3210',
+       email: 'jane.smith@example.com',
+       status: 'INACTIVE'
+     },
+     {
+       id: 3,
+       name: 'Alice Johnson',
+       address: '789 Oak St',
+       phone: '555-123-4567',
+       email: 'alice.johnson@example.com',
+       status: 'ACTIVE'
+     }
+   ];
+   client: ClientI | null = null;
 
 }
